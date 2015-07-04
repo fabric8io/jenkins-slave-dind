d082e832d7a988f7c653437ae7c2b6178047545e
Ioannis Canellos <iocanel@gmail.com>
2015-07-04 17:31:25 +0300

    Copy jenkins keys

--- start-jenkins-slave.sh
+++ start-jenkins-slave.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 echo "export DOCKER_HOST=unix:///var/run/docker.sock" >> /home/jenkins/.profile
+
+#copy ssh keys
+mkdir -p /home/jenkins/.ssh
+chmod 700 /home/jenkins/.ssh
+cp /home/jenkins/ssh-keys/authorized_keys /home/jenkins/.ssh/authorized_keys
+chown -R jenkins:jenkins /home/jenkins/.ssh/
+chmod 600 /home/jenkins/.ssh/authorized_keys
 #start docker in docker
 nohup wrapdocker &
 
