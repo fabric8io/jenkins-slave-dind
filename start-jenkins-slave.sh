6c9db8e36e2e1c715d319820006955560b3d217d
Ioannis Canellos <iocanel@gmail.com>
2015-07-08 16:48:29 +0300

    Minor change

--- start-jenkins-slave.sh
+++ start-jenkins-slave.sh
@@ -6,7 +6,7 @@ echo "export DOCKER_HOST=unix:///var/run/docker.sock" >> /home/jenkins/.profile
 mkdir -p /home/jenkins/.ssh
 chmod 700 /home/jenkins/.ssh
 cp /home/jenkins/ssh-keys/authorized_keys /home/jenkins/.ssh/authorized_keys
-chown -R jenkins:jenkins /home/jenkins/.ssh/
+chown -R jenkins:jenkins /home/jenkins/.ssh
 chmod 600 /home/jenkins/.ssh/authorized_keys
 #start docker in docker
 nohup wrapdocker &
